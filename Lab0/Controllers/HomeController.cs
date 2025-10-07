05d929c564195d318bdd57d730580bb4e2f38358
jakub.piechowicz <jakub.piechowicz@microsoft.wsei.edu.pl>
2025-10-07 18:22:45 +0200

    Lab01 end

--- Lab0/Controllers/HomeController.cs
+++ Lab0/Controllers/HomeController.cs
@@ -18,11 +18,57 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Age(DateTime date)
+    {
+        ViewBag.Result = DateTime.Now.Year - date.Year ;
+        return View();
+    }
+    
     public IActionResult Privacy()
     {
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Calculator(double? x, double? y, [FromQuery(Name = "operator")]string op)
+    {
+       //  string? x = Request.Query["x"];
+       // string? y = Request.Query["y"];
+       // string? op = Request.Query["op"];
+       // double xval = Convert.ToDouble(x);
+       // double yval = Convert.ToDouble(y);
+       // double result = xval + yval;
+       // ViewBag.Result = result; 
+       if (x is null || y is null)
+       {
+           
+           return View("CalculatorError", "Brak wartości x lub y!");
+       }
+       switch (op)
+       {
+           case"add":
+               ViewBag.Result = $"{x} + {y} = {x + y}";
+               break;
+           case"sub":
+               ViewBag.Result = $"{x} - {y} = {x - y}";
+               break;
+           case"mul":
+               ViewBag.Result = $"{x} * {y} = {x * y}";
+               break;
+           case"div":
+               ViewBag.Result = $"{x} / {y} = {x / y}";
+               break;
+           default:
+               ViewBag.Result = "Error";
+               break;
+       }
+       
+        return View();
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
